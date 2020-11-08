b33e78275489537fa5b8081e34352c6bbe19238d
Joseph Racca <briseph@Josephs-iMac.local>
2020-11-07 21:08:36 -0800

    updates to 100

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// alert('Hello!');
-
 let voteCount1 = document.getElementById("voteCount1");
 let votebtn1 = document.getElementById("votebtn1");
 let votePlural1 = document.getElementById("votePlural1");
@@ -7,84 +5,73 @@ let votePlural1 = document.getElementById("votePlural1");
 let totalVotes1 = 0;
 let clicked1 = false;
 
-votebtn1.addEventListener("click", function () {
-  //    first click will set bool to true
-
-  if (clicked1 == false) {
-    clicked1 = true;
-    totalVotes1++;
-    voteCount1.innerText = totalVotes1;
-    votebtn1.classList = "red";
-  } else {
-    clicked1 = false;
-    totalVotes1--;
-    voteCount1.innerText = totalVotes1;
-    votebtn1.classList = "gray";
-  }
-
-  //    function will do the following: should be + or -
-  //    totalVotes1++;
-
-  if (totalVotes1 == 1) {
-    votePlural1.innerText = "VOTE";
-  } else {
-    votePlural1.innerText = "VOTES";
-  }
-});
-
 let voteCount2 = document.getElementById("voteCount2");
 let votebtn2 = document.getElementById("votebtn2");
+let downvotebtn = document.getElementById("downvotebtn");
 let votePlural2 = document.getElementById("votePlural2");
 
 let totalVotes2 = 0;
 let clicked2 = false;
 
-votebtn2.addEventListener("click", function () {
-  //    first click will set bool to true
+let inputToggle = document.getElementById("inputToggle");
 
-  if (clicked2 == false) {
-    clicked2 = true;
-    totalVotes2++;
-    voteCount2.innerText = totalVotes2;
-    votebtn2.classList = "red";
+inputToggle.addEventListener("click", function () {
+  if (inputField.classList.contains("d-none")) {
+    inputField.classList.remove("d-none");
+    inputToggle.innerText = "CANCEL";
   } else {
-    clicked2 = false;
+    inputField.classList.add("d-none");
+    inputToggle.innerText = "COMMENT";
+  }
+});
+
+downvotebtn.addEventListener("click", function () {
+  if (totalVotes2 > 0) {
     totalVotes2--;
     voteCount2.innerText = totalVotes2;
-    votebtn2.classList = "gray";
+    downvotebtn.classList.add = "red";
+  } else {
   }
+  voteTextDisplay();
+});
 
-  //    function will do the following: should be + or -
-  //    totalVotes2++;
+votebtn2.addEventListener("click", function () {
+  if (totalVotes2 < 100) {
+    totalVotes2++;
+    voteCount2.innerText = totalVotes2;
+    votebtn2.classList = "green";
+    downvotebtn.classList.add = "d-none";
+  } else {
+    voteCount2.innerText = totalVotes2;
+  }
+  voteTextDisplay();
+});
 
+voteTextDisplay = function () {
   if (totalVotes2 == 1) {
     votePlural2.innerText = "VOTE";
+  } else if (totalVotes2 == 100) {
+    votePlural2.innerText = "VOTES (MAX)";
   } else {
     votePlural2.innerText = "VOTES";
   }
-});
+}
 
-let comment1 = document.getElementById('comment1');
-let commentToggle1 = document.getElementById('commentToggle1')
+let comment1 = document.getElementById("comment1");
+let commentToggle1 = document.getElementById("commentToggle1");
 
-commentToggle1.addEventListener
-
-('click', function(){
-//we want to target comment1 or comment div
-// let showComments = true;
-if(comment1.classList.contains('d-none')){
-    comment1.classList.remove('d-none');
+commentToggle1.addEventListener("click", function () {
+  //we want to target comment1 or comment div
+  // let showComments = true;
+  if (comment1.classList.contains("d-none")) {
+    comment1.classList.remove("d-none");
     commentToggle1.innerText = "HIDE COMMENTS";
-    
-}
-else{
+  } else {
     //remove d-none
-    comment1.classList.add('d-none');
+    comment1.classList.add("d-none");
     commentToggle1.innerText = "SHOW COMMENTS";
-}
-
+  }
 });
-
 
 //  NOTE: if this were a real website, we would want to send that information to the main server so that it could tally up the counts, but only once per unique user
 
